9b97a43c7d8c7f3d66d128c3b5f0968941678fe9
broman1122 <gabr.daijl@edufalkenberg.se>
2026-02-06 10:28:50 +0100

    Add files via upload

---
+++ -New userscript-.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-12-16
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.tampermonkey.net/scripts.php
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
