007b221ae3f21289846b4433cec7a403c3aa44a8
broman1122 <gabr.daijl@edufalkenberg.se>
2026-02-06 10:32:56 +0100

    Add initial userscript template

--- -New userscript-.user.js
+++ -New userscript-.user.js
@@ -1,16 +1,1 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      2025-12-16
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.tampermonkey.net/scripts.php
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-
-    // Your code here...
-})();
+,,
